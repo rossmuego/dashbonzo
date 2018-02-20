471d000eea3e0aa101ce0f65b9ff3617be499c47
Ross Muego <rossmuego@gmail.com>
2018-02-20 12:54:51 +0000

    updated url

--- js/monzo.js
+++ js/monzo.js
@@ -1,3 +1,3 @@
 function auth() {
-  window.location.replace('https://auth.monzo.com/?client_id=oauthclient_00009TXCnYQLZeF8s0rvbF&redirect_uri=https://file:///Users/rossmuego/Documents/Uni/Side%20Projects/dashbonzo/index.html&response_type=code');
+  window.location.replace('https://auth.monzo.com/?client_id=oauthclient_00009TXCnYQLZeF8s0rvbF&redirect_uri=https://rossmuego.github.io/dashbonzo/index.html&response_type=code');
 }
