1e936e4dddad4cfc95f83209b79949252d180bbb
Ross Muego <rossmuego@gmail.com>
2018-02-22 18:11:29 +0000

    Added today transactions

--- js/monzo.js
+++ js/monzo.js
@@ -8,12 +8,13 @@ function init() {
   var transactions = getTransactions(accounts);
   var balance = getBalance(accounts);
 
+  console.log(transactions);
   document.getElementById('account-balance').innerHTML = "£" + balance.balance / 100;
 
   for (var i = transactions.transactions.length - 1; i > 0; i--) {
 
     if (transactions.transactions[i].merchant == null) {
-      merch = "Not Merchant"
+      merch = "N/A"
     } else {
       merch = transactions.transactions[i].merchant.name;
     }
@@ -28,7 +29,35 @@ function init() {
       singleTrans.style.backgroundColor = "green";
     }
     singleTrans.innerHTML = transactions.transactions[i].amount / 100 + '       ' + merch
-    document.getElementById('transactions').appendChild(singleTrans);
+    document.getElementById('transactions-all').appendChild(singleTrans);
+  }
+
+  var today = new Date(Date.now()).toISOString();
+  today = today.slice(0, 10);
+  console.log(today)
+
+  for (var i = transactions.transactions.length - 1; i > 0; i--) {
+
+    if (transactions.transactions[i].created.slice(0,10) == today) {
+
+      if (transactions.transactions[i].merchant == null) {
+        merch = "N/A"
+      } else {
+        merch = transactions.transactions[i].merchant.name;
+      }
+
+      var singleTrans = document.createElement('div')
+      singleTrans.id = transactions.transactions[i].id
+      singleTrans.className = "transaction";
+
+      if (parseInt((transactions.transactions[i].amount)) < 0) {
+        singleTrans.style.backgroundColor = "red";
+      } else {
+        singleTrans.style.backgroundColor = "green";
+      }
+      singleTrans.innerHTML = transactions.transactions[i].amount / 100 + '       ' + merch
+      document.getElementById('transactions-today').appendChild(singleTrans);
+    }
   }
 }
 
